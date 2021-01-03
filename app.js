c857766420f36f9da7725462c230828311398f15
kmuni08 <Karishma.Muni24@gmail.com>
2021-01-03 16:38:42 -0500

    ready for production again

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use((error, req, res, next) => {
 mongoose
     .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vbxke.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false })
     .then(() => {
-        app.listen(5000);
+        app.listen(process.env.PORT|| 5000);
     })
     .catch(err => {
         console.log(err);
